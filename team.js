9db8cdad7df91ec02e9ec32ab500b464cad1454f
Manuel <manu.benitez1@hotmail.com>
2023-09-28 09:20:30 +0200

    agregue mis datos

--- team.js
+++ team.js
@@ -1,12 +1,14 @@
-let members = [{
-name: "",
-surname: "",
-age: "",
-city: "",
-hobby: "",
-favoriteFood: "",
-favoriteVideoGame: "",
-favoriteFilm: "",
-favoriteBook: "",
-petName: "",
-}]
+let members = [
+  {
+    name: "Manuel",
+    surname: "Benitez",
+    age: "29",
+    city: "Palma",
+    hobby: "Skate",
+    favoriteFood: "Milanesa",
+    favoriteVideoGame: "DOTA 2",
+    favoriteFilm: "",
+    favoriteBook: "Shantaram",
+    petName: "",
+  },
+];
